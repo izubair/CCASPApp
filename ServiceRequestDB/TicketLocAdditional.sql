9fbf4c7bb4c7db1ff4659262cc746fdc30fc1127
izubair <irfanzub@hotmail.com>
2017-11-29 16:18:11 -0800

    added the database project to the solution and to the folder
    Added the missing database project to folder and to solution

---
+++ ServiceRequestDB/TicketLocAdditional.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TicketLocAdditional]
+(
+	[TicketLocAddId] INT NOT NULL PRIMARY KEY,
+	[TicketId] INT FOREIGN KEY references Tickets(TicketId),
+	[JurisdictionCode] INT NOT NULL DEFAULT 1,
+	[MinorCivilDiv] INT NOT NULL DEFAULT 1,
+	[StateAssembly] INT NOT NULL DEFAULT 1,
+	[StateSenate] INT NOT NULL DEFAULT 1,
+	[CityWard] INT NOT NULL DEFAULT 1,
+	[CommissionDist] INT NOT NULL DEFAULT 1,
+	[Commissioner] INT NOT NULL DEFAULT 1
+)
